3a51ae4618c8f56b1f18f4fe1949441966a4b973
miguelgarciaibanez <mgarciaibanez@gmail.com>
2019-06-05 00:24:22 +0200

    Env variable mongo

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,7 @@ let urlDB;
 // if (process.env.NODE_ENV === 'dev'){
 //     urlDB = 'mongodb://localhost:27017/cafe';
 // } else {
-     urlDB = 'mongodb+srv://mgarciaibanez:QsGEo6CFswKWjsC4@tfmcluster-ejih2.mongodb.net/cafe';
+     urlDB = process.env.mongoURI;
 // }
 
 process.env.URLDB = urlDB;
